e3535aa256e04771012dfdfd9a4e00d64e71459d
nazneen-patan-01 <patannazneen089@gmaill.com>
2025-02-06 00:34:03 +0530

    added code

---
+++ swap-two-numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  
+int a,b,temp;
+scanf("%d",&a);
+scanf("%d",&b);
+ temp=a;//1
+ a=b;//3
+ b=temp;//1
+ printf("a = %d\n",a);
+  printf("b = %d\n",b); 
+ 
+
+    return 0;
+}
