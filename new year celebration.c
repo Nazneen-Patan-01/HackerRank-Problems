99e9cc6e5f03ed388abaf28fdb73eca880d58a36
Nazneen <patannazneen089@gmail.com>
2025-03-05 00:15:11 +0530

    Create new year celebration.c

---
+++ new year celebration.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    
+    int budget, numGuests, foodCostPerGuest, decorationCost, musicCost, extraExpenses;
+    scanf("%d %d %d %d %d %d", &budget, &numGuests, &foodCostPerGuest, &decorationCost, &musicCost, &extraExpenses);
+
+    int totalFoodCost = numGuests * foodCostPerGuest;
+    int totalCost = totalFoodCost + decorationCost + musicCost + extraExpenses;
+
+    if (totalCost <= budget && numGuests > 5 && numGuests <= 50 &&
+        (decorationCost < 0.3 * budget || totalFoodCost < 0.5 * budget) &&
+        (numGuests <= 25 || musicCost > 0))
+      {
+        printf("Celebration Approved\n");
+      } 
+     else 
+     {
+        printf("Celebration Denied\n");
+     }
+
+    return 0;
+}
